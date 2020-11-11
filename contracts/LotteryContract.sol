faf81ad88bfc43a61b5ec35fb8d0c83278501955
Akshay Sharma <akzarma@gmail.com>
2020-11-11 13:33:14 +0530

    Random number VRF intgeration

--- contracts/LotteryContract.sol
+++ contracts/LotteryContract.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract LotteryContract {
-    
+contract LotteryContract is VRFConsumerBase {
     struct RulesConfig {
         uint256 numOfWinners;
         uint256 playerLimit;
@@ -23,11 +23,41 @@ contract LotteryContract {
     LotteryStatus lotteryStatus;
     RulesConfig rulesConfig;
 
-    constructor() public
+    bytes32 internal keyHashVRF;
+    uint256 internal feeVRF;
+    uint256 public randomResult;
+
+    constructor()
+        public
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088 // LINK Token
+        )
     {
         adminAddress = msg.sender;
         lotteryStatus = LotteryStatus.NOTSTARTED;
         totalLotteryPool = 0;
+
+        keyHashVRF = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        feeVRF = 0.1 * 10**18; // 0.1 LINK
+    }
+
+    function getRandomNumber(uint256 userProvidedSeed)
+        public
+        returns (bytes32 requestId)
+    {
+        require(
+            LINK.balanceOf(address(this)) > feeVRF,
+            "Not enough LINK - fill contract with faucet"
+        );
+        return requestRandomness(keyHashVRF, feeVRF, userProvidedSeed);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        randomResult = randomness;
     }
 
     function setLotteryRules(
@@ -37,7 +67,10 @@ contract LotteryContract {
         uint256 adminFeePercentage,
         address tokenAddress
     ) public {
-        require(msg.sender == adminAddress, "Starting the Lottery requires Admin Access");
+        require(
+            msg.sender == adminAddress,
+            "Starting the Lottery requires Admin Access"
+        );
         require(
             lotteryStatus == LotteryStatus.NOTSTARTED,
             "Starting the Lottery requires Admin Access"
@@ -59,7 +92,10 @@ contract LotteryContract {
             "Max Participation for the Lottery Reached"
         );
         //add condition if address has already entered
-        require(lotteryStatus == LotteryStatus.INPROGRESS, "The Lottery is Closed");
+        require(
+            lotteryStatus == LotteryStatus.INPROGRESS,
+            "The Lottery is Closed"
+        );
         require(
             lotteryToken.allowance(msg.sender, address(this)) >=
                 rulesConfig.registrationAmount,
@@ -76,13 +112,14 @@ contract LotteryContract {
         return true;
     }
 
-
-       function settleLottery() internal {
+    function settleLottery() internal {
         require(msg.sender == adminAddress, "Requires Admin Access");
-        uint256 adminFees = (totalLotteryPool * rulesConfig.adminFeePercentage)/100;
-        uint256 winnersPool = (totalLotteryPool - adminFees) /  rulesConfig.numOfWinners;
+        uint256 adminFees = (totalLotteryPool *
+            rulesConfig.adminFeePercentage) / 100;
+        uint256 winnersPool = (totalLotteryPool - adminFees) /
+            rulesConfig.numOfWinners;
         //temporary logic till we put the random number logic
-        uint winningIndex = 0;
+        uint256 winningIndex = 0;
         winners.push(lotteryPlayers[winningIndex]);
         for (uint256 i = 0; i < rulesConfig.numOfWinners; i++) {
             address winnerAddress = winners[i];
@@ -96,11 +133,9 @@ contract LotteryContract {
         lotteryToken.transferFrom(address(this), adminAddress, adminFees);
         lotteryStatus = LotteryStatus.CLOSED;
     }
-    
 
     //add logic to reset contract variblaes values
     // save historic data of Lottery
     //error handling
     //random number integration and making sure that the contract always has LINK tokens
-
 }
